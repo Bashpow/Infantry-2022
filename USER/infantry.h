debfc4ccb990a753c006487794af47c92e59433f
Bashpow <9009400+Bashpow@user.noreply.gitee.com>
2021-05-25 04:01:09 +0800

    删除多余代码，更新版本号

--- USER/infantry.h
+++ USER/infantry.h
@@ -6,10 +6,9 @@
 #include "stm32f4xx.h"
 #include "usart3.h"
 
-#define VERSION "1.0"
+#define VERSION "1.1"
 
 //云台GM6020电机头角度
-// #define YAW_GM6020_HEAD_ANGLE 2735
 #define YAW_GM6020_HEAD_ANGLE 5420
 
 #endif
